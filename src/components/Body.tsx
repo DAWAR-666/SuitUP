2a820c731803615ec46551dac7c8a22780c9efef
DAWAR <dawarpratham8@gmail.com>
2026-01-25 17:03:50 +0530

    created body component and appStore

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -1,8 +1,23 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import Login from "./Login"
+import Browser from "./Browser"
 
 
 const Body = () => {
+    const appRouter=createBrowserRouter([
+      {
+        path:"/",
+        element:<Login/>
+      },
+      {
+        path:"/browse",
+        element:<Browser />
+      }  
+    ])
   return (
-    <div>Body</div>
+    <div>
+        <RouterProvider router={appRouter} />
+    </div>
   )
 }
 
