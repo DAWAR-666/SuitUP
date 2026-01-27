718dc6669eb4dddb471eaf666ce266e4d3fd1187
DAWAR <dawarpratham8@gmail.com>
2026-01-27 19:27:06 +0530

    updated css

--- src/components/Browser.tsx
+++ src/components/Browser.tsx
@@ -4,13 +4,16 @@ import MenuOptions from "./MenuOptions"
 
 const Browser = () => {
   return (
-    <div className="bg-[url(bgLogo.png)] bg-repeat w-screen h-screen">
-      <Header/>
-      
-        
-        <MenuOptions/> 
-        <ImgBox/> 
-      
+    <div className="bg-[url(bgLogo.png)] bg-repeat w-screen h-screen grid grid-cols-12 grid-rows-12 gap-20">
+      <div className="col-span-full row-span-1">
+        <Header/>
+      </div>
+      <div className=" col-start-1 row-start-3">
+        <MenuOptions/>
+      </div>
+      <div className="col-end-12 row-start-3 ">
+        <ImgBox/>
+      </div>   
     </div>
   )
 }
--- src/components/MenuOptions.tsx
+++ src/components/MenuOptions.tsx
@@ -1,9 +1,13 @@
 const MenuOptions= () => {
   return (
     <div
-    className="absolute mt-40 w-55/100 bg-black h-72/100 text-white"
+    className="absolute w-55/100 bg-black/50 h-72/100 rounded-r-4xl grid grid-cols-1 grid-rows-1"
     >
-        MenuOPT
+        <div className="m-5 ml-0 border-white border-4 border-l-0 rounded-r-4xl col-span-full row-span-full">
+          <h1 className="text-center text-3xl font-bold m-2">Craft Your Cool</h1>
+          <h1 className="mx-3 text-2xl">What's Your Fit?</h1>
+          <div className="m-5 h-full">form input </div>
+        </div>
     </div>
   )
 }
