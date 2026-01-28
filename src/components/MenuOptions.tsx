6a42dd7905fb51161272c197d246270a15d9808a
DAWAR <dawarpratham8@gmail.com>
2026-01-28 19:03:58 +0530

    updated buttons for dimensions and suit type

--- src/components/MenuOptions.tsx
+++ src/components/MenuOptions.tsx
@@ -1,6 +1,8 @@
-
+import SuitTypes from './SuitTypes'
 import Dimensions from './Dimensions'
+import { useState } from 'react'
 const MenuOptions= () => {
+  const [step,setStep]=useState<number>(0);
   
   return (
     <div
@@ -8,10 +10,21 @@ const MenuOptions= () => {
     >
         <div className="m-5 ml-0 border-white border-4 border-l-0 rounded-r-4xl col-span-full row-span-full grid grid-cols-12 grid-rows-12">
           <h1 className="text-center text-3xl font-bold p-1 mb-1 row-span-1 col-start-4 col-end-10 bg-white rounded-b-xl">Craft Your Cool</h1>
+          {(step===0)?<Dimensions/>:<SuitTypes/>}
           
-          <Dimensions/>
-          <button className="bg-white col-start-2 col-end-6 m-1 text-xl cursor-pointer border-2 border-white rounded-4xl">{'<-'} prev</button>
-          <button className=" bg-white m-1 col-start-8 col-end-12 text-xl cursor-pointer border-2 border-white rounded-4xl">next {'->'}</button>
+          {step===1&&(<button 
+            className="bg-white col-start-2 col-end-6 row-start-11 m-1 text-xl cursor-pointer border-2 border-white rounded-4xl"
+            onClick={()=>setStep(0)}
+            
+          >
+              {'<-'} prev
+          </button>)}
+          <button 
+            className=" bg-white m-1 col-start-8 col-end-12 row-start-11 text-xl cursor-pointer border-2 border-white rounded-4xl"
+            onClick={()=>(setStep(1))}
+          >
+              {step===1?`SUBMIT`:`next ->`}
+          </button>
         </div>
     </div>
   )
---
+++ src/components/SuitTypes.tsx
@@ -0,0 +1,8 @@
+
+const SuitTypes = () => {
+  return (
+    <div>SuitTypes</div>
+  )
+}
+
+export default SuitTypes
